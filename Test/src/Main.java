4093654b3d3579572b9694d1c52a799b75b8203b
Joaquin Roco Fuenzalida <166854417+Jrgitx@users.noreply.github.com>
2025-03-27 14:17:16 -0300

    Add files via upload

---
+++ Test/src/Main.java
@@ -0,0 +1,23 @@
+
+    public class Main {
+        public class BigVigenere{
+            int [] key;
+            char[][] alphabet;
+            //sii
+        }
+        public static void main(String[] args) {
+            //TIP Press have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    
+
+            //<shortcut>actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+            System.out.println("Hello and welcome!");
+            System.out.println();
+    //GUARDADO
+            for (int i = 1; i <= 5; i++){
+                System.out.println("i = " + i);
+                //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We
+            }
+        }
+    }
